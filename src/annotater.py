402dd61a2b2810e1e6d6e7a6ba8b8eae70a63a5b
Nick Kocharhook <nick@kocharhook.com>
2024-07-07 13:28:16 +0100

    Driving the browser works!

--- src/annotater.py
+++ src/annotater.py
@@ -1,8 +1,10 @@
+import readchar
 import typer
+import undetected_chromedriver as uc
 from dotenv import load_dotenv
-
+from selenium import webdriver
 from typing_extensions import Annotated
-
+from selenium.webdriver.chrome.options import Options
 
 load_dotenv()
 
@@ -20,10 +22,45 @@ def annotate_ed_desc_images(
 
 class Annotater:
     def __init__(self, debug):
-        pass
+        # chrome_options = uc.ChromeOptions()
+        # chrome_options.user_data_dir = "selenium"
+        # chrome_options.add_argument("user-data-dir=selenium")
+        # options=chrome_options
+        self.driver = uc.Chrome(user_data_dir="selenium")
 
     def process(self):
-        print("do stuff")
+        self.driver.get(
+            "https://www.familysearch.org/ark:/61903/3:1:3QHV-R32D-GPLS?i=493&cat=1037259"
+        )
+
+        while True:
+            key = readchar.readkey()
+            print(key)
+
+            match key:
+                case x if "0" <= x <= "9":
+                    print("digit")
+                case "[":
+                    self.prevImage()
+                case "]":
+                    self.nextImage()
+                case "q":
+                    break
+
+    def nextImage(self):
+        self.clickSpanWithClass("next")
+
+    def prevImage(self):
+        self.clickSpanWithClass("previous")
+
+    def clickSpanWithClass(self, name):
+        self.driver.execute_script(
+            f"""
+            var span = window.document.getElementsByClassName('{name}')[0];
+            var click = new Event('click');
+            span.dispatchEvent(click);
+        """
+        )
 
 
 if __name__ == "__main__":
