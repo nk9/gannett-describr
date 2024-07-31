213fbc49f6c2edb5314a45ea71d00ced457fd17a
Nick Kocharhook <nick@kocharhook.com>
2024-07-31 16:55:20 +0100

    Scraper works!

--- src/scraper.py
+++ src/scraper.py
@@ -1,18 +1,27 @@
+import base64
 import csv
 import json
+import logging
+import os
+import random
 import re
 import sqlite3
-from pathlib import Path
-from pyrate_limiter import Duration, Limiter, RequestRate
-import os
-import logging
 import textwrap
+import time
+from pathlib import Path
+
 import typer
 from dotenv import load_dotenv
+from pyrate_limiter import Duration, Limiter, RequestRate
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from typing_extensions import Annotated
 
 from src.be_nice import CachedLimiterSession
 from src.driver import driver
+from src.ed import Ed
+from src.store import Store
+from src.utils import buildImageList
 
 # Enable logging for Requests, etc
 # logging.basicConfig(level=logging.DEBUG)
@@ -27,88 +36,100 @@ app = typer.Typer()
 @app.command()
 def scrape_ed_desc_images(
     debug: Annotated[bool, typer.Option("--debug", "-v")] = False,
+    headless: Annotated[bool, typer.Option("--headless", "-h")] = False,
 ):
-    scraper = Scraper(debug)
+    scraper = Scraper(debug, driver(headless))
     scraper.scrape_ed_desc_images()
     # scraper.write(Path("../gannett-data/fs_eds.parquet"))
 
 
 class Scraper:
-    def __init__(self, debug):
+    def __init__(self, debug, driver):
         self.debug = debug
+        self.driver = driver
 
         db_path = "annotated.db"
         self.connection = sqlite3.connect(f"file:{db_path}?mode=ro", uri=True)
         self.cursor = self.connection.cursor()
+        self.store = Store(self.cursor, buildImageList())
 
-        rate = RequestRate(2, Duration.SECOND * 61)
-        limiter = Limiter(rate)
-
-        self.session = CachedLimiterSession(
-            cache_name="scrape_fs_ed_desc",
-            allowable_codes=[200],
-            allowable_methods=["GET", "HEAD", "POST"],
-            limiter=limiter,
-        )
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.71 Safari/537.36",
-            "Cookie": os.getenv("FS_COOKIE"),
-            # "Authorization": os.getenv("FS_AUTH"),
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        }
+        # rate = RequestRate(2, Duration.SECOND * 61)
+        # limiter = Limiter(rate)
 
     def scrape_ed_desc_images(self):
-        arks = self.cursor.execute(
-            "SELECT year, utp_code, ark FROM images ORDER BY id"
-        ).fetchall()
+        # arks = self.cursor.execute(
+        #     "SELECT year, utp_code, ark, cat FROM images ORDER BY id"
+        # ).fetchall()
         out_path = Path("../ed-desc-img/1930/")
-        count = len(arks)
+        # count = len(arks)
 
-        for i, (
-            year,
-            utp_code,
-            ark,
-        ) in enumerate(arks):
-            short_ark = ark[4:]
-            ark_path = out_path / str(year) / utp_code / f"{short_ark}.jpg"
+        # Let the user sign in
+        self.driver.get(
+            "https://www.familysearch.org/auth/familysearch/login?returnUrl=https%3A%2F%2Fwww.familysearch.org%2Fen%2Fhome%2Fportal%2F"
+        )
+
+        val = input("Waiting… [q to quit] ")
+
+        if val == "q":
+            return
+
+        while img := self.nextImage():
+            short_ark = img.ark[4:]
+            ark_path = out_path / str(img.year) / img.utp_code / f"{short_ark}.png"
             ark_path.parent.mkdir(parents=True, exist_ok=True)
-            if not ark_path.is_file():
-                url = ED_DESC_URL.format(ark)
-                headers = self.headers | {
-                    "Referer": f"https://www.familysearch.org/ark:/61903/{ark}?cat=1037259",
-                    "Accept-Encoding": "gzip, deflate, br",
-                    "Priority": "u=0, i",
-                    "Sec-Ch-Ua": '"Not/A)Brand";v="8", "Chromium";v="126"',
-                    "Sec-Fetch-Site": "same-origin",
-                    "Sec-Fetch-Mode": "navigate",
-                    "Sec-Fetch-Dest": "document",
-                    "Sec-Fetch-User": "?1",
-                    "Sec-Ch-Ua-Mobile": "?0",
-                    "Sec-Ch-Ua-Platform": '"macOS"',
-                    "Accept-Language": "en-GB",
-                    "Upgrade-Insecure-Requests": "1",
-                }
-                print(f"### Fetching {ark}")
-                res = self.session.get(
-                    url,
-                    headers=headers,
-                    # hooks={"response": print_roundtrip},
-                    allow_redirects=False,
-                )
-                print("###", res.status_code)
-                if res.status_code == 302:
-                    redir_url = "https://www.familysearch.org" + res.headers["Location"]
-                    res2 = self.session.get(
-                        redir_url,
-                        headers=headers,
-                        # hooks={"response": print_roundtrip}
+            last_3 = "/".join(ark_path.parts[-3:])
+
+            if not ark_path.is_file() or ark_path.stat().st_size < 50_000:
+                self.driver.get(img.url)
+                time.sleep(10)  # Let the image fully load
+
+                try:
+                    canvas = self.driver.find_element(
+                        By.CSS_SELECTOR, ".openseadragon-canvas canvas"
                     )
-                    if res2.status_code == 200:
-                        with open(ark_path, "wb") as f:
-                            print(f"### [{i:5}/{count}] Writing {short_ark}.jpg")
-                            f.write(res2.content)
-                    else:
-                        logging.warning(f"### Failed with code {res.status_code}")
+                    data_url = self.driver.execute_script(
+                        "return arguments[0].toDataURL()", canvas
+                    )
+                    header, encoded = data_url.split(",", 1)
+                    image_data = base64.b64decode(encoded)
+
+                    with open(ark_path, "wb") as file:
+                        print(f"Writing  {last_3}")
+                        file.write(image_data)
+
+                except Exception as e:
+                    logging.warn(f"Failed to find download link for {img.ark}")
+
+                time.sleep(random.randint(10, 45))
+            else:
+                print(f"Skipping {last_3}")
+
+    def nextImage(self):
+        old = self.store.curr()
+        new = next(self.store)
+        is_first = self.store.index == 1
+
+        if new:
+            if (
+                old.utp_code == new.utp_code
+                and new.image_index == old.image_index + 1
+                and not is_first  #
+            ):
+                self.clickSpanWithClass("next")
+            else:
+                self.driver.get(new.url)
+            return new
+
+        return None
+
+    def clickSpanWithClass(self, name):
+        self.driver.execute_script(
+            f"""
+            var span = window.document.getElementsByClassName('{name}')[0];
+            var click = new Event('click');
+            span.dispatchEvent(click);
+        """
+        )
 
 
 def print_roundtrip(response, *args, **kwargs):
